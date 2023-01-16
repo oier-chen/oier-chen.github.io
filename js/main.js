3100f7d95181c6f56f1a67e20784522f572a5ef8
chenyuxuan <2176807108@qq.com>
2023-01-16 20:20:02 +0800

    修整音乐

--- js/main.js
+++ js/main.js
@@ -136,34 +136,34 @@ $('#hitokoto').click(function () {
 //获取天气
 //请前往 https://www.mxnzp.com/doc/list 申请 app_id 和 app_secret
 //请前往 https://dev.qweather.com/ 申请 key
-const add_id = "spntjcq1nnnkwft6"; // app_id
-const app_secret = "TkpzVkMvWkOyTVNxYIhQaGxGQUo2QTO9"; // app_secret
-const key = "abaa1a70101b49fc926a431ea9ce73ab" // key
-function getWeather() {
-    fetch("https://www.mxnzp.com/api/ip/self?app_id=" + add_id + "&app_secret=" + app_secret)
-        .then(response => response.json())
-        .then(data => {
-            let str = data.data.city
-            let city = str.replace(/市/g, '')
-            $('#city_text').html(city);
-            fetch("https://geoapi.qweather.com/v2/city/lookup?location=" + city + "&number=1&key=" + key)
-                .then(response => response.json())
-                .then(location => {
-                    let id = location.location[0].id
-                    fetch("https://devapi.qweather.com/v7/weather/now?location=" + id + "&key=" + key)
-                        .then(response => response.json())
-                        .then(weather => {
-                            $('#wea_text').html(weather.now.text)
-                            $('#tem_text').html(weather.now.temp + "°C&nbsp;")
-                            $('#win_text').html(weather.now.windDir)
-                            $('#win_speed').html(weather.now.windScale + "级")
-                        })
-                })
-        })
-        .catch(console.error);
-}
+// const add_id = "spntjcq1nnnkwft6"; // app_id
+// const app_secret = "TkpzVkMvWkOyTVNxYIhQaGxGQUo2QTO9"; // app_secret
+// const key = "abaa1a70101b49fc926a431ea9ce73ab" // key
+// function getWeather() {
+//     fetch("https://www.mxnzp.com/api/ip/self?app_id=" + add_id + "&app_secret=" + app_secret)
+//         .then(response => response.json())
+//         .then(data => {
+//             let str = data.data.city
+//             let city = str.replace(/市/g, '')
+//             $('#city_text').html(city);
+//             fetch("https://geoapi.qweather.com/v2/city/lookup?location=" + city + "&number=1&key=" + key)
+//                 .then(response => response.json())
+//                 .then(location => {
+//                     let id = location.location[0].id
+//                     fetch("https://devapi.qweather.com/v7/weather/now?location=" + id + "&key=" + key)
+//                         .then(response => response.json())
+//                         .then(weather => {
+//                             $('#wea_text').html(weather.now.text)
+//                             $('#tem_text').html(weather.now.temp + "°C&nbsp;")
+//                             $('#win_text').html(weather.now.windDir)
+//                             $('#win_speed').html(weather.now.windScale + "级")
+//                         })
+//                 })
+//         })
+//         .catch(console.error);
+// }
 
-getWeather();
+// getWeather();
 
 let wea = 0;
 $('#upWeather').click(function () {
